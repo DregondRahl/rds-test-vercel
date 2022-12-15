66ae14c8b27f2a71a590acba4ffe9972ec0b5263
Niranjith Perumal <niranjithperumal@cunet.carleton.ca>
2022-12-14 20:46:00 -0500

    fix(build): vercel builds with npm

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,6 +28,7 @@ export default function Home() {
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
+            <Button title='RDS-HELLO' onClick={() => alert('RDS HELLO')}/>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
